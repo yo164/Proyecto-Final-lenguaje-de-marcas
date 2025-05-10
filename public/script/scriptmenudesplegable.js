710785884b9c506b3004624d2f3684c7441037ff
yo164 <fcobsan076@g.educaand.es>
2025-05-10 02:11:04 +0200

    menu desplegable ccasi completo

--- public/script/scriptmenudesplegable.js
+++ public/script/scriptmenudesplegable.js
@@ -1,32 +1,47 @@
 const links = document.querySelectorAll('.enlaces');
 const principal = document.getElementById('principal');
 
+let menuDesplegable = null;
+
 links.forEach(link => {
     link.addEventListener('mouseenter', () => {
         console.log('focused', link.textContent);
+
+        if(menuDesplegable){
+            menuDesplegable.remove();
+        }
         const submenu = document.createElement('div');
         submenu.classList.add('desplegable');
         submenu.innerHTML = `
+        <h4>${link.textContent}</h4>
         <nav>
         <a>Subcat1</a>
         <a>Subcat2</a>
         </nav>
         `;
 
-        const linkRect = link.getBoundingClientRect();
-        submenu.style.top = `${linkRect.top}px`;
-        submenu.style.left = `${linkRect.right}px`;
+
 
 
         principal.appendChild(submenu);
+
+        menuDesplegable = submenu;
     });
 
     link.addEventListener('mouseleave', () => {
         console.log('desfocused', link.textContent);
 
-        const submenu = link.querySelector('.desplegable');
-        if(submenu) {
+        const submenu = principal.querySelector('.desplegable');
+        if(submenu && !submenu.contains(event.relatedTarget)) {
             submenu.remove();
+            menuDesplegable = null;
+        }
+    });
+
+    principal.addEventListener('mouseleave', (event) => {
+        if (menuDesplegable && !menuDesplegable.contains(event.relatedTarget)) {
+            menuDesplegable.remove();
+            menuDesplegable = null;
         }
     });
 });
