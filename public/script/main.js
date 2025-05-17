f787a0272f51cea2e0073bc91df666bfc9ecc761
yo164 <fcobsan076@g.educaand.es>
2025-05-17 05:06:55 +0200

    comienzo del front end para llamar a los productos al catalogo principal

--- public/script/main.js
+++ public/script/main.js
@@ -1,5 +1,8 @@
 const url = 'http://localhost:3000/api/productos';
 
 fetch(url).then(response =>  {
-    if(!response.ok) throw new Error('Error al obtener los productos');
+    if(!response.ok) {
+        throw new Error('Error al obtener los productos');
+    } 
+    return response.json(); 
 })
