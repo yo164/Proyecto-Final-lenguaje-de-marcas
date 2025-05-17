b8790a408a2ed774b9c5d7d681a4226b494c952a
yo164 <fcobsan076@g.educaand.es>
2025-05-17 05:32:08 +0200

    primer front end exitoso muestra todos los productos de la base de datos

--- public/script/main.js
+++ public/script/main.js
@@ -5,4 +5,21 @@ fetch(url).then(response =>  {
         throw new Error('Error al obtener los productos');
     } 
     return response.json(); 
-})
+}).then(data => {
+    const contenedor = document.getElementById('contenedor');//aqui se selecciona el id del contenedor donde se mostrarán
+    data.forEach(producto => {
+        const item = document.createElement('div');//creamos el div que contendra los datos del producto
+        item.classList.add('contenedorProducto');//le damos nombre a la clase del div(revisa el html para ver bien los nombres)
+        //aqui relleno el interior del div
+        //como falta la foto pondre lo basico en varios p y a volar
+        item.innerHTML = `
+        <h3>${producto.nombreProducto}</h3>
+        <p>Precio: ${producto.precio}</p>
+        <p>Descripción: ${producto.descripcion}</p>
+        
+        `;
+        contenedor.appendChild(item);
+    });
+}).catch(error => {
+    console.error('Huboun error:', error);
+});
