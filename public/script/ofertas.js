30a002bc5136e083c672a4e82f180a9ca94aee61
yo164 <fcobsan076@g.educaand.es>
2025-05-18 11:39:09 +0200

    cambios en css y html para separar footer y header de los css de los contenidos de cada pagina además de front para mostrar productos por la banda de ofertas

---
+++ public/script/ofertas.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        const res = await fetch('http://localhost:3000/api/productos?categoria=Rebajas');
+        const productos = await res.json();
+
+        const contenedorBanda = document.querySelector('.bandaofertas');
+
+        productos.slice(0, 10).forEach((producto, index) => {
+            
+            const div = document.createElement('div');
+            div.classList.add('producto');
+            div.style.animationDelay = `${index * 8}s`; // index empieza en 0 multiplica *8 cada vuelta
+            div.style.zIndex = `${index + 1000}`;//suma con un zindex inicial alto para asegurar supersposicion
+            //y además al ir aumentando sobre pone uno al otro
+            div.innerHTML = `
+            
+                <div class="contenedorImg">
+                    <img src="${producto.urlImagen}" alt="Foto de ${producto.nombreProducto}" class="imgProducto">
+                </div>
+                <div class="contenedorContenido">
+                    <h3>${producto.nombreProducto}</h3>
+                    <p>Precio: ${producto.precio}</p>
+                    <p>Descripción: ${producto.descripcion}</p>
+                </div>
+                <div class="contenedorBotones">
+                   
+                    <button type="submit" class="botonsubmitOferta">
+                        Añadir al Carrito
+                    </button>
+                </div>
+        
+            `;
+
+            contenedorBanda.appendChild(div);
+        });
+
+    } catch (error) {
+        console.error('Error al cargar productos de rebajas:', error);
+    }
+});
